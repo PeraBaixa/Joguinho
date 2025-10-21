ea4516e02075ab6c80789060753c0f9831529b24
Éric Peralta <50339718+PeraBaixa@users.noreply.github.com>
2025-10-20 21:51:40 -0300

    Adicionei algumas armas e uma armadura incompleta

--- Itens.py
+++ Itens.py
@@ -14,3 +14,38 @@ class PotMente(Util):
     
     def efeito(obj):
         return [[2, "MEN"], [1, "VON"]]
+
+
+#Armas
+class MachadoManeiro(Arma):
+    def __init__(obj):
+        super().__init__("Machado Maneiro", 3, "Força", 4)
+
+class EscudoDeMesa(Arma):
+    def __init__(obj):
+        super().__init__("Escudo de Mesa", 1, "Resistência", 3)
+
+class BolasMentais(Arma):
+    def __init__(obj):
+        super().__init__("Esferas de controle remoto", 1, "Concentração", 3)
+
+class FacaNinja(Arma):
+    def __init__(obj):
+        super().__init__("Faca Ninja", 3, "Furtividade", 1)
+
+class MarteloAssustador(Arma):
+    def __init__(obj):
+        super().__init__("Martelo Assustador", 2, "Intimidação", 2)
+
+#Arrumar essa arma:
+class Erro(Arma):
+    def __init__(obj):
+        super().__init__("????", 0, "ENT", 2)
+
+    def getDano(obj)
+        return obj.danoBase
+
+#Armaduras
+class Kevlar(Armadura):
+    def __init__(obj):
+        super().__init__("Kevlar")
