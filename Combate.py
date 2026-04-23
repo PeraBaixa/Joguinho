8f67ab804541ed14351239f23bb6c4692adca2c4
Eric <eric.p3r4lt4@gmail.com>
2026-04-23 15:19:16 -0300

    Começando a acrescentar o frontend

---
+++ Combate.py
@@ -0,0 +1,94 @@
+import Criaturas, Itens
+import Personagem
+from random import randrange
+
+Personagem.iniAtr([8, 6, 4, 10])
+Personagem.armadura = "A1"
+nerd = Criaturas.Criatura()
+
+def criaOrdem(ini):
+    ordem = [ini[0]]
+    for mon in ini[1:]:
+        for i, o in enumerate(ordem):
+            if o.atrs["AGI"] <= mon.atrs["AGI"]:
+                ordem.insert(i, mon)
+                break
+        else:
+            ordem.append(mon)
+    
+    #Adiciona o protagonista à ordem:
+    for i, op in enumerate(ordem):
+        if Personagem.atrs["AGI"] >= op.atrs["AGI"]:
+            ordem.insert(i, 0)
+            break
+    else:
+        ordem.append(0)
+    
+    return ordem
+
+#Função provisória para criar combate
+def combate(*inimigos):
+    inivivo = list(inimigos)
+    ataque, defesa = None, None
+    arma = Itens.retornaItem(Personagem.arma)
+    armadura = Itens.retornaItem(Personagem.armadura) 
+
+    while Personagem.vida[1] > 0 and len(inivivo) > 0:
+        print(f"{Personagem.nome}: {Personagem.vida[1]} pv", end="")
+        [print(f"\t{i.nome}: {i.vida[1]} pv", end="") for i in inimigos]
+        print()
+
+        for ini in criaOrdem(inivivo):
+            if ini:
+                ataque = ini.atacar()
+                print(f"{ini.nome} ataca, dando {ataque[0]} de dano")
+                if "defesa" in Personagem.condicoes and armadura:
+                    ataque += (armadura.getResis(), )
+                    Personagem.condicoes.remove("defesa")
+                else:
+                    ataque += (0, )
+                Personagem.sofrerDano(ataque)
+                if Personagem.vida[1] <= 0: break
+            #Vez do personagem
+            else:
+                print(f"Sua vez, {Personagem.nome}!")
+                print("1 - Atacar")
+                print("2 - Defender")
+                try:
+                    opt = int(input("Decida sua ação: "))
+                except:
+                    opt = None
+
+                match opt:
+                    #Atacar
+                    case 1:
+                        if len(inivivo) > 1:
+                            [print(f"{n+1} - {i.nome}") for n,i in enumerate(inivivo)]
+                            try:
+                                alvo = inivivo[int(input("Escolha qual inimigo atacar: "))-1]
+                            except:
+                                alvo = inivivo[0]
+                        else:
+                            alvo = inivivo[0]
+                        ataque = Personagem.atacar(arma)
+                        print(f"{Personagem.nome} ataca, dando {ataque[0]+ataque[1]} de dano")
+                        alvo.sofrerDano(ataque)
+                        if alvo.vida[1] <= 0: inivivo.remove(alvo)
+                        if len(inivivo) <= 0: break
+                    #Defender
+                    case 2:
+                        print(f"{Personagem.nome} se defende!")
+                        Personagem.condicoes.append("defesa")
+                    #Fugir
+                    case __:
+                        pass
+        print()
+    
+    else:
+        print(f"{Personagem.nome}: {Personagem.vida[1]} pv", end="")
+        [print(f"\t{i.nome}: {i.vida[1]} pv", end="") for i in inimigos]
+        print()
+        if Personagem.vida[1] > 0:
+            print(f"Parabéns, {Personagem.nome} venceu o combate!")
+        else:
+            print(f"infelizmente, {Personagem.nome} perdeu miseravelmente")
--- Main.py
+++ Main.py
@@ -1,97 +1,121 @@
-import Criaturas, Itens
-import Personagem
+from turtle import Screen
+import pygame
 from random import randrange
+import os
 
-Personagem.iniAtr([8, 6, 4, 10])
-Personagem.armadura = "A1"
-nerd = Criaturas.Criatura()
+pygame.init()
+pygame.display.set_caption("Hora da Escola")
 
+#Constantes gerais
+LARGURA, ALTURA = 800, 600
+FPS = 60
+TELA = pygame.display.set_mode((LARGURA, ALTURA))
+CLOCK = pygame.time.Clock()
 
-def criaOrdem(ini):
-    ordem = [ini[0]]
-    for mon in ini[1:]:
-        for i, o in enumerate(ordem):
-            if o.atrs["AGI"] <= mon.atrs["AGI"]:
-                ordem.insert(i, mon)
-                break
+#Constantes de cores:
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+CINZA = (128, 128, 128)
+AZUL = (0, 0, 255)
+CIANO = (0, 255, 255)
+VERDE = (0, 255, 0) #Mais especificamente, verde-limão
+VERMELHO = (255, 0, 0)
+AMARELO = (255, 255, 0)
+LARANJA = (255, 165, 0)
+ROXO = (128, 0, 128)
+
+FONTEBOTAO = pygame.font.Font(None, 40)
+BOTAORECT = pygame.Rect(10, 480, 385, 50) #Tamanho padrão de um botão
+
+class Botao:
+    def __init__(obj, texto, acao, loc=None, corB=BRANCO, corT=PRETO):
+        obj.texto = FONTEBOTAO.render(texto, True, corT)
+        obj.dimen = BOTAORECT.copy()
+        if loc: obj.dimen.topleft = loc
+        obj.acao = acao
+        obj.cor = corB
+        obj.clicado = False
+
+    def verificaClique(obj, mPos):
+        x, y = mPos
+        checkX = (obj.dimen.x <= x <= (obj.dimen.x + obj.dimen.width))
+        checkY = (obj.dimen.y <= y <= (obj.dimen.y + obj.dimen.height))
+
+        if checkX and checkY and pygame.mouse.get_pressed()[0]:
+            return True
         else:
-            ordem.append(mon)
+            return False
     
-    #Adiciona o protagonista à ordem:
-    for i, op in enumerate(ordem):
-        if Personagem.atrs["AGI"] >= op.atrs["AGI"]:
-            ordem.insert(i, 0)
-            break
+    def verificaSobre(obj, mPos):
+        x, y = mPos
+        checkX = (obj.dimen.x <= x <= (obj.dimen.x + obj.dimen.width))
+        checkY = (obj.dimen.y <= y <= (obj.dimen.y + obj.dimen.height))
+
+        return (checkX and checkY)
+
+    def desenhar(obj):
+        pygame.draw.rect(TELA, obj.cor, obj.dimen)
+        TELA.blit(obj.texto, obj.texto.get_rect(center=obj.dimen.center))
+
+def testa():
+    print("Teste")
+
+def carrega_imagem(arq, tam):
+    if os.path.exists(arq):
+        img = pygame.image.load(arq).convert_alpha()
+        img = pygame.transform.scale(img, tam)
     else:
-        ordem.append(0)
+        img = pygame.Surface(tam)
+        img.fill((255, 255, 255))
     
-    return ordem
+    return img
 
-#Função provisória para criar combate
-def combate(*inimigos):
-    inivivo = list(inimigos)
-    ataque, defesa = None, None
-    arma = Itens.retornaItem(Personagem.arma)
-    armadura = Itens.retornaItem(Personagem.armadura) 
+def movimenta(tecla, loc):
+    if tecla[pygame.K_a] and (loc.x - 5) >= 0:
+        loc.x -= 5
+    if tecla[pygame.K_d] and (loc.x + 5) <= (LARGURA - 100):
+        loc.x += 5
+    if tecla[pygame.K_w] and (loc.y - 5) >= 0:
+        loc.y -= 5
+    if tecla[pygame.K_s] and (loc.y + 5) <= (ALTURA - 200):
+        loc.y += 5
 
-    while Personagem.vida[1] > 0 and len(inivivo) > 0:
-        print(f"{Personagem.nome}: {Personagem.vida[1]} pv", end="")
-        [print(f"\t{i.nome}: {i.vida[1]} pv", end="") for i in inimigos]
-        print()
+def main():
+    jogador = carrega_imagem("Sprites/Protagonista.png", (200, 250))
+    jogloc = jogador.get_rect()
+    menuComb = []
+    menuComb.append(Botao("Atacar", testa))
+    menuComb.append(Botao("Defender", testa, loc=(405, 480)))
+    menuComb.append(Botao("Itens", testa, loc=(10, 540)))
+    menuComb.append(Botao("Fugir", testa, loc=(405, 540)))
 
-        for ini in criaOrdem(inivivo):
-            if ini:
-                ataque = ini.atacar()
-                print(f"{ini.nome} ataca, dando {ataque[0]} de dano")
-                if "defesa" in Personagem.condicoes and armadura:
-                    ataque += (armadura.getResis(), )
-                    Personagem.condicoes.remove("defesa")
-                else:
-                    ataque += (0, )
-                Personagem.sofrerDano(ataque)
-                if Personagem.vida[1] <= 0: break
-            #Vez do personagem
+    while True:
+        CLOCK.tick(FPS)
+        TELA.fill((0, 0, 0))
+        
+        mPos = pygame.mouse.get_pos()
+        teclas = pygame.key.get_pressed()
+        movimenta(teclas, jogloc)
+
+        for botao in menuComb:
+            if botao.verificaSobre(mPos):
+                botao.cor = CINZA
             else:
-                print(f"Sua vez, {Personagem.nome}!")
-                print("1 - Atacar")
-                print("2 - Defender")
-                try:
-                    opt = int(input("Decida sua ação: "))
-                except:
-                    opt = None
+                botao.cor = BRANCO
+            botao.desenhar()
+            if botao.verificaClique(mPos):
+                botao.acao()
+                pygame.time.wait(300)
 
-                match opt:
-                    #Atacar
-                    case 1:
-                        if len(inivivo) > 1:
-                            [print(f"{n+1} - {i.nome}") for n,i in enumerate(inivivo)]
-                            try:
-                                alvo = inivivo[int(input("Escolha qual inimigo atacar: "))-1]
-                            except:
-                                alvo = inivivo[0]
-                        else:
-                            alvo = inivivo[0]
-                        ataque = Personagem.atacar(arma)
-                        print(f"{Personagem.nome} ataca, dando {ataque[0]+ataque[1]} de dano")
-                        alvo.sofrerDano(ataque)
-                        if alvo.vida[1] <= 0: inivivo.remove(alvo)
-                        if len(inivivo) <= 0: break
-                    #Defender
-                    case 2:
-                        print(f"{Personagem.nome} se defende!")
-                        Personagem.condicoes.append("defesa")
-                    #Fugir
-                    case __:
-                        pass
-        print()
-    
-    else:
-        print(f"{Personagem.nome}: {Personagem.vida[1]} pv", end="")
-        [print(f"\t{i.nome}: {i.vida[1]} pv", end="") for i in inimigos]
-        print()
-        if Personagem.vida[1] > 0:
-            print(f"Parabéns, {Personagem.nome} venceu o combate!")
-        else:
-            print(f"infelizmente, {Personagem.nome} perdeu miseravelmente")
+        
+        TELA.blit(jogador, jogloc)
 
-combate(Criaturas.Criatura())
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or teclas[pygame.K_ESCAPE]:
+                pygame.quit()
+                return
+        
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
